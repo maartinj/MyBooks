61436fab7a1917019745bb19d7295cf209aeeded
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-08-19 17:19:17 +0200

    first commit: load initial files

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Marcin Jędrzejak on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
