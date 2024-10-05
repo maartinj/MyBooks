6920c487835071cd676b46bbcc9be9ff9a1dd2ee
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-10-05 20:18:09 +0200

    feat: Read

--- MyBooks/Book.swift
+++ MyBooks/Book.swift
@@ -5,7 +5,7 @@
 //  Created by Marcin Jędrzejak on 19/08/2024.
 //
 
-import Foundation
+import SwiftUI
 import SwiftData
 
 @Model
@@ -37,6 +37,17 @@ class Book {
         self.summary = summary
         self.rating = rating
         self.status = status
+    }
+    
+    var icon: Image {
+        switch status {
+            case .onShelf:
+                Image(systemName: "checkmark.diamond.fill")
+            case .inProgress:
+                Image(systemName: "book.fill")
+            case .completed:
+                Image(systemName: "books.vertical.fill")
+        }
     }
 }
 
