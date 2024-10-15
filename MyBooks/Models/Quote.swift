14729409f22535108c179a0a51c3e726e2eed714
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-10-15 14:51:12 +0200

    feat: Required Code Modifications

--- MyBooks/Models/Quote.swift
+++ MyBooks/Models/Quote.swift
@@ -11,7 +11,7 @@ import SwiftData
 @Model
 class Quote {
     var creationDate: Date = Date.now
-    var text: String
+    var text: String = ""
     var page: String?
     
     init(text: String, page: String? = nil) {
