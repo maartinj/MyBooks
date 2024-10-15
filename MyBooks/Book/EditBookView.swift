7b9be03f80d0f06fe2e227193db57c5817fa4fda
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-10-15 23:02:45 +0200

    feat: Adding a PhotoPicker

--- MyBooks/Book/EditBookView.swift
+++ MyBooks/Book/EditBookView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditBookView: View {
     @Environment(\.dismiss) private var dismiss
@@ -20,6 +21,8 @@ struct EditBookView: View {
     @State private var dateCompleted = Date.distantPast
     @State private var recommendedBy = ""
     @State private var showGenres = false
+    @State private var selectedBookCover: PhotosPickerItem?
+    @State private var selectedBookCoverData: Data?
     
     init(book: Book) {
         self.book = book
@@ -85,20 +88,54 @@ struct EditBookView: View {
                 }
             }
             Divider()
-            LabeledContent {
-                RatingsView(maxRating: 5, currentRating: $rating, width: 30)
-            } label: {
-                Text("Rating")
-            }
-            LabeledContent {
-                TextField("", text: $title)
-            } label: {
-                Text("Title").foregroundStyle(.secondary)
-            }
-            LabeledContent {
-                TextField("", text: $author)
-            } label: {
-                Text("Author").foregroundStyle(.secondary)
+            HStack {
+                PhotosPicker(
+                    selection: $selectedBookCover,
+                    matching: .images,
+                    photoLibrary: .shared()) {
+                        Group {
+                            if let selectedBookCoverData,
+                               let uiImage = UIImage(data: selectedBookCoverData) {
+                                Image(uiImage: uiImage)
+                                    .resizable()
+                                    .scaledToFit()
+                            } else {
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .tint(.primary)
+                            }
+                        }
+                        .frame(width: 75, height: 100)
+                        .overlay(alignment: .bottomTrailing) {
+                            if selectedBookCoverData != nil {
+                                Button {
+                                    selectedBookCover = nil
+                                    selectedBookCoverData = nil
+                                } label: {
+                                    Image(systemName: "x.circle.fill")
+                                        .foregroundStyle(.red)
+                                }
+                            }
+                        }
+                    }
+                VStack {
+                    LabeledContent {
+                        RatingsView(maxRating: 5, currentRating: $rating, width: 30)
+                    } label: {
+                        Text("Rating")
+                    }
+                    LabeledContent {
+                        TextField("", text: $title)
+                    } label: {
+                        Text("Title").foregroundStyle(.secondary)
+                    }
+                    LabeledContent {
+                        TextField("", text: $author)
+                    } label: {
+                        Text("Author").foregroundStyle(.secondary)
+                    }
+                }
             }
             LabeledContent {
                 TextField("", text: $recommendedBy)
@@ -166,6 +203,11 @@ struct EditBookView: View {
             dateStarted = book.dateStarted
             dateCompleted = book.dateCompleted
             recommendedBy = book.recommendedBy
+        }
+        .task(id: selectedBookCover) {
+            if let data = try? await selectedBookCover?.loadTransferable(type: Data.self) {
+                selectedBookCoverData = data
+            }
         }
     }
     
